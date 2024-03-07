966b4ef20b68069abe85aebeba4fb045dec5bfa7
Lukas Polak <admin@lukaspolak.cz>
2024-03-07 16:26:26 +0100

    add lab3

--- RazorWebPageCloud/RazorWebPageCloud/Pages/Index.cshtml.cs
+++ RazorWebPageCloud/RazorWebPageCloud/Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -14,7 +16,26 @@ namespace RazorWebPageCloud.Pages
 
         public void OnGet()
         {
+            Blobs = CreateClient().GetBlobs().ToList();
+        }
 
+        // normally we would add this to the Azure cfg...
+        private const string AzureStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=............;EndpointSuffix=core.windows.net";
+        private const string AzureStorageContainerName = "...";
+
+        public List<BlobItem>? Blobs { get; set; }
+
+        public FileStreamResult OnGetDownloadBlob(string blobName)
+        {
+            var blobClient = CreateClient().GetBlobClient(blobName);
+            var contentType = blobClient.GetProperties().Value.ContentType;
+            var stream = blobClient.OpenRead(); // není tøeba øešit Dispose - stream nám uzavøe infrastruktura Razor Pages.
+            return new FileStreamResult(stream, contentType);
+        }
+
+        private BlobContainerClient CreateClient()
+        {
+            return new Azure.Storage.Blobs.BlobContainerClient(AzureStorageConnectionString, AzureStorageContainerName);
         }
     }
 }
